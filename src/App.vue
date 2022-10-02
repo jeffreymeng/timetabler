be6c491eb4aa8a9bf88db4c1b8408be54ee8f3e9
Jeffrey Meng <jeffkmeng@gmail.com>
2022-10-01 23:09:37 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -31,8 +31,9 @@ const center = { lat: 51.093048, lng: 6.84212 };
       <GMapMap
         :center="center"
         :zoom="7"
-        map-type-id="terrain"
-        style="width: 100vw; height: 900px"
+        gesture-handling="greedy"
+        
+        
       >
       </GMapMap>
     </div>
