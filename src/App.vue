a1c275c48760e78cacf06f2a9e88a0ae9eae596c
Nathan Wang <nathan.r.wang@gmail.com>
2022-10-01 22:58:23 -0400

    more ui

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,37 @@
 import draggable from "vuedraggable";
 import { ref } from "vue";
 
-const drag = ref(false);
 const myArray = ref([
-  { name: "Jeffery", id: 0 },
-  { name: "Nathan", id: 1 },
-  { name: "Jason", id: 2 },
+  { name: "10355 Tonita Way, Cupertino, CA 95014", id: 0 },
+  { name: "22562 Alcade Road, Cupertino, CA 95014", id: 1 },
+  { name: "12345 Rainbow Drive, Cupertino, CA 95014", id: 2 },
 ]);
 
-const center = {lat: 51.093048, lng: 6.842120};
+const center = { lat: 51.093048, lng: 6.84212 };
 </script>
 
 <template>
   <div class="h-full flex">
     <div className="w-96">
-      My Sidebar
-      <draggable
-        v-model="myArray"
-        group="people"
-        @start="drag = true"
-        @end="drag = false"
-        item-key="id"
-        ghost-class="ghost"
-      >
+      <div className="m-4 font-bold text-lg">Destinations</div>
+      <draggable v-model="myArray" item-key="id" ghost-class="ghost">
         <template #item="{ element }">
-          <div>{{ element.name }}</div>
+          <div class="m-2">
+            <input
+              v-model="element.name"
+              class="p-2 rounded w-full border border-gray-200"
+            />
+          </div>
         </template>
       </draggable>
     </div>
 
     <div class="flex-1">
       <GMapMap
-          :center="center"
-          :zoom="7"
-          map-type-id="terrain"
-          style="width: 100vw; height: 900px"
+        :center="center"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 100vw; height: 900px"
       >
       </GMapMap>
     </div>
